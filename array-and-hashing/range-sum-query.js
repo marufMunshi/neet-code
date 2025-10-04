c67f461dd5df4cfb762906f04f0e3b5a642d723b
marufMunshi <marufhasanmunshi@gmail.com>
2025-10-04 13:52:09 +0600

    feat: solve range sum query

---
+++ array-and-hashing/range-sum-query.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/range-sum-query-immutable
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.nums = nums;
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  let sum = 0;
+
+  for (let index = left; index <= right; index++) {
+    sum = sum + this.nums[index];
+  }
+
+  return sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
