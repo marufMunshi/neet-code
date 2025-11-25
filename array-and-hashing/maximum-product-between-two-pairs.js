5d1edc513a26b32d4c7286b799075f08ee47e2ff
marufMunshi <marufhasanmunshi@gmail.com>
2025-11-25 09:47:00 +0600

    feat: solve max product difference

---
+++ array-and-hashing/maximum-product-between-two-pairs.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/maximum-product-difference-between-two-pairs/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function (nums) {
+  nums.sort((a, b) => b - a);
+
+  return nums[0] * nums[1] - nums[nums.length - 1] * nums[nums.length - 2];
+};
